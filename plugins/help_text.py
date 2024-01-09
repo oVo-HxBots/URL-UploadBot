dfe1b246b68462c62e307050bcaff76f27b4dbf7
Kamlesh Saini <88968064+oVo-HxBots@users.noreply.github.com>
2024-01-09 10:17:30 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -14,12 +14,12 @@ from plugins.startmsg import Translation
 
 from pyrogram import filters
 from database.adduser import AddUser
-from pyrogram import Client as Clinton
+from pyrogram import Client as Hxbots
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-@Clinton.on_message(filters.private & filters.command(["cancel"]))
+@Hxbots.on_message(filters.private & filters.command(["cancel"]))
 async def cancel_process(bot, update):
     save_ytdl_json_path = Config.DOWNLOAD_LOCATION + "/" + str(update.chat.id) + ".json"
     if os.path.exists(save_ytdl_json_path):
@@ -40,7 +40,7 @@ async def cancel_process(bot, update):
             reply_to_message_id=update.message_id
         )
 
-@Clinton.on_message(filters.private & filters.reply & filters.text)
+@Hxbots.on_message(filters.private & filters.reply & filters.text)
 async def edit_caption(bot, update):
     try:
         await bot.send_cached_media(
@@ -60,7 +60,7 @@ async def edit_caption(bot, update):
         except:
             pass
 
-@Clinton.on_message(filters.private & filters.command(["help"]))
+@Hxbots.on_message(filters.private & filters.command(["help"]))
 async def help_user(bot, update):
     # logger.info(update)
     await AddUser(bot, update)
@@ -79,7 +79,7 @@ async def help_user(bot, update):
        reply_to_message_id=update.message_id
      )
 
-@Clinton.on_message(filters.private & filters.command(["caption"]))
+@Hxbots.on_message(filters.private & filters.command(["caption"]))
 async def add_caption_help(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
@@ -88,7 +88,7 @@ async def add_caption_help(bot, update):
         reply_to_message_id=update.message_id
     )
 
-@Clinton.on_message(filters.private & filters.command(["about"]))
+@Hxbots.on_message(filters.private & filters.command(["about"]))
 async def about(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
@@ -104,7 +104,7 @@ async def about(bot, update):
        reply_to_message_id=update.message_id
      )
 
-@Clinton.on_message(filters.private & filters.command(["start"]))
+@Hxbots.on_message(filters.private & filters.command(["start"]))
 async def start(bot, update):
     # logger.info(update)
     await AddUser(bot, update)
@@ -125,7 +125,7 @@ async def start(bot, update):
       reply_to_message_id=update.message_id
     )
 
-@Clinton.on_message(filters.private & filters.command(["info"]))
+@Hxbots.on_message(filters.private & filters.command(["info"]))
 async def add_info_help(bot, update):
     if update.from_user.last_name:
         last_name = update.from_user.last_name
@@ -137,3 +137,8 @@ async def add_info_help(bot, update):
         #parse_mode="html",
         reply_to_message_id=update.message_id
     )
+  
+@Hxbots.on_message(filters.command("restart"))
+async def restart_handler(bot, update):
+    await m.reply_text("Bot Successfully Restarted!", True)
+    os.execl(sys.executable, sys.executable, *sys.argv)
