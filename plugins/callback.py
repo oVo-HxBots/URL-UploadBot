b515ba62bbfe141416bcb14c412d618b1f23d239
Kamlesh Saini <88968064+oVo-HxBots@users.noreply.github.com>
2024-01-09 10:14:02 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -1,16 +1,16 @@
 
 import os
 from pyrogram import filters
-from pyrogram import Client as Clinton
+from pyrogram import Client as Hxbots
 from plugins.youtube_dl_button import youtube_dl_call_back
 from plugins.dl_button import ddl_call_back
 from plugins.hx import hx_call_back
 
-@Clinton.on_callback_query(filters.regex('^X0$'))
+@Hxbots.on_callback_query(filters.regex('^X0$'))
 async def delt(bot, update):
     await update.message.delete(True)
 
-@Clinton.on_callback_query()
+@Hxbots.on_callback_query()
 async def button(bot, update):
     cb_data = update.data
     if "|" in cb_data:
