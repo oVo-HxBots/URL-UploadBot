6af88656a424958d6652236dd3c9dc5c7cab25e5
Kamlesh Saini <88968064+oVo-HxBots@users.noreply.github.com>
2024-01-09 10:38:39 +0530

    Create txt_dl.py

---
+++ plugins/txt_dl.py
@@ -0,0 +1,346 @@
+import requests
+import json
+import subprocess
+from pyrogram import Client,filters
+from pyrogram.types.messages_and_media import message
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors import FloodWait
+from pyromod import listen
+from pyrogram.types import Message
+import pyrogram
+from pyrogram import Client as bot, filters
+import tgcrypto
+from p_bar import progress_bar
+from subprocess import getstatusoutput
+import helper
+import logging
+import time
+import aiohttp
+import asyncio
+import aiofiles
+from pyrogram.types import User, Message
+# import progressor 
+# from progressor import progress_for_pyrogram
+import sys
+import re
+import os
+from os import environ
+# import pycurl
+
+@bot.on_message(filters.command(["txt_dl"]))
+async def account_login(bot: Client, m: Message):
+    editable = await m.reply_text("**Hi!, I Can Download All Links In A Txt File & Send Them To You.\n\nUse /help To Get Some Help 😉\n\n Now Send .txt file**")
+    input: Message = await bot.listen(editable.chat.id)
+    x = await input.download()
+    await input.delete(True)
+
+    path = f"./downloads/{m.chat.id}"
+
+    try:    
+        with open(x, "r") as f:
+            content = f.read()
+        content = content.split("\n")
+        links = []
+        for i in content:
+            links.append(i.split(":", 1))
+        os.remove(x)
+        # print(len(links))
+    except:
+        await m.reply_text("Invalid file input.")
+        os.remove(x)
+        return
+
+    editable = await m.reply_text(f"Total links found are **{len(links)}**\n\nSend From where you want to download initial is **0**")
+    input1: Message = await bot.listen(editable.chat.id)
+    raw_text = input1.text
+
+
+    try:
+        arg = int(raw_text)
+    except:
+        arg = 0
+    
+    
+    editable = await m.reply_text("**Enter Title For File. Send 'de' To Use Default File Name.**")
+    input0: Message = await bot.listen(editable.chat.id)
+    raw_text0 = input0.text
+    
+    await m.reply_text("**`Enter Resolution \nExamples: | 144 | 180 | 240 | 360 | 480 | 720 |\n Without | |`**")
+    input2: Message = await bot.listen(editable.chat.id)
+    raw_text2 = input2.text
+    
+    await editable.edit("**Enter Your Name or send `de` for use default file Name**")
+
+    input3: Message = await bot.listen(editable.chat.id)
+    raw_text3 = input3.text
+    if raw_text3 == 'de':
+        CR = credit
+    else:
+        CR = raw_text3
+
+    await m.reply_text("Now send the **Thumb url**\nEg : ```https://telegra.ph/Abhi-04-08```\n\nor Send **no**")
+  
+    input4: Message = await bot.listen(editable.chat.id)
+    raw_text4 = input6.text
+
+    thumb = input4.text
+    if thumb.startswith("http://") or thumb.startswith("https://"):
+        getstatusoutput(f"wget '{thumb}' -O 'thumb.jpg'")
+        thumb = "thumb.jpg"
+    else:
+        thumb == "no"
+        
+    if raw_text =='0':
+        count =1
+    else:       
+        count =int(raw_text)    
+
+    
+    try:
+        for i in range(arg, len(links)):
+            
+            url = links[i][1]
+            name1 = links[i][0].replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@","").replace("*","").replace(".","").strip()
+
+
+
+            if raw_text2 =="144":
+
+                cmd = f'yt-dlp -F "{url}"'
+                k = await helper.run(cmd)
+                out = helper.vid_info(str(k))
+                # print(out)
+                if '256x144' in out:
+                    ytf = f"{out['256x144']}"
+                elif '320x180' in out:
+                    ytf = out['320x180']    
+                elif 'unknown' in out:
+                    ytf = out["unknown"]
+                else:
+                    ytf = "no"
+
+            elif raw_text2 =="180":
+
+                cmd = f'yt-dlp -F "{url}"'
+                k = await helper.run(cmd)
+                out = helper.vid_info(str(k))
+                # print(out)              
+                if '320x180' in out:
+                    ytf = out['320x180']
+                elif '426x240' in out:
+                    ytf = out['426x240']
+                elif 'unknown' in out:
+                    ytf = out["unknown"]
+                else:
+                    ytf = "no"                        
+
+            elif raw_text2 =="240":
+                
+                cmd = f'yt-dlp -F "{url}"'
+                k = await helper.run(cmd)
+                out = helper.vid_info(str(k))
+                # print(out)          
+                if '426x240' in out:
+                    ytf = out['426x240']
+                elif '426x234' in out:
+                    ytf = out['426x234']
+                elif '480x270' in out:
+                    ytf = out['480x270']
+                elif '480x272' in out:
+                    ytf = out['480x272']
+                elif '640x360' in out:
+                    ytf = out['640x360']
+                elif 'unknown' in out:
+                    ytf = out["unknown"]
+                else:
+                    ytf = "no"
+
+            elif raw_text2 =="360":
+            
+                cmd = f'yt-dlp -F "{url}"'
+                k = await helper.run(cmd)
+                out = helper.vid_info(str(k))
+                # print(out)             
+                if '640x360' in out:
+                    ytf = out['640x360']
+                elif '638x360' in out:
+                    ytf = out['638x360']
+                elif '636x360' in out:
+                    ytf = out['636x360']
+                elif '768x432' in out:
+                    ytf = out['768x432']
+                elif '638x358' in out:
+                    ytf = out['638x358']
+                elif '852x316' in out:
+                    ytf = out['852x316']
+                elif '850x480' in out:
+                    ytf = out['850x480']
+                elif '848x480' in out:
+                    ytf = out['848x480']
+                elif '854x480' in out:
+                    ytf = out['854x480']
+                elif '852x480' in out:
+                    ytf = out['852x480']
+                elif '854x470' in out:
+                    ytf = out['852x470']  
+                elif 'unknown' in out:
+                    ytf = out["unknown"]
+                else:
+                    ytf = "no"
+
+            elif raw_text2 =="480":
+                
+                cmd = f'yt-dlp -F "{url}"'
+                k = await helper.run(cmd)
+                out = helper.vid_info(str(k))
+                # print(out)               
+                if '854x480' in out:
+                    ytf = out['854x480']
+                elif '852x480' in out:
+                    ytf = out['852x480']                        
+                elif '854x470' in out:
+                    ytf = out['854x470']  
+                elif '768x432' in out:
+                    ytf = out['768x432']
+                elif '848x480' in out:
+                    ytf = out['848x480']
+                elif '850x480' in out:
+                    ytf =['850x480']
+                elif '960x540' in out:
+                    ytf = out['960x540']
+                elif '640x360' in out:
+                    ytf = out['640x360']   
+                elif 'unknown' in out:
+                    ytf = out["unknown"]                     
+                else:
+                    ytf = "no"
+
+                   
+            elif raw_text2 =="720":
+                
+                cmd = f'yt-dlp -F "{url}"'
+                k = await helper.run(cmd)
+                out = helper.vid_info(str(k))
+                # print(out)           
+                if '1280x720' in out:
+                    ytf = out['1280x720'] 
+                elif '1280x704' in out:
+                    ytf = out['1280x704'] 
+                elif '1280x474' in out:
+                    ytf = out['1280x474'] 
+                elif '1920x712' in out:
+                    ytf = out['1920x712'] 
+                elif '1920x1056' in out:
+                    ytf = out['1920x1056']    
+                elif '854x480' in out:
+                    ytf = out['854x480']                        
+                elif '640x360' in out:
+                    ytf = out['640x360']     
+                elif 'unknown' in out:
+                    ytf = out["unknown"]              
+                else:
+                    ytf = "no"
+            elif "player.vimeo" in url:
+                if raw_text2 == '144':
+                    ytf= 'http-240p'
+                elif raw_text2 == "240":
+                    ytf= 'http-240p'
+                elif raw_text2 == '360':
+                    ytf= 'http-360p'
+                elif raw_text2 == '480':
+                    ytf= 'http-540p'
+                elif raw_text2 == '720':
+                    ytf= 'http-720p'
+                else:
+                    ytf = 'http-360p'
+            else:
+                ytf="no"
+
+            if ytf == "0":
+                res = "0"
+            elif ytf == "no" or ytf == "mp4":
+                res = "NA"
+            else:
+                res = list(out.keys())[list(out.values()).index(ytf)]
+
+            name = f'{name1}'
+
+
+            if "youtu" in url:
+                cmd = f'yt-dlp -o "{name}.%(ext)s" -f "bestvideo[height<={int(raw_text2)}]+bestaudio" --no-keep-video --remux-video mp4 "{url}"'
+            elif "player.vimeo" in url:
+                cmd = f'yt-dlp -f "{ytf}+bestaudio" --no-keep-video --remux-video mp4 "{url}" -o "{name}.%(ext)s"'
+            elif "m3u8" or "livestream" in url:
+                cmd = f'yt-dlp -f "{ytf}" --no-keep-video --remux-video mp4 "{url}" -o "{name}.%(ext)s"'
+            elif ytf == "0" or "unknown" in out:
+                cmd = f'yt-dlp -f "{ytf}" --no-keep-video --remux-video mp4 "{url}" -o "{name}.%(ext)s"'
+            elif ".pdf" in url:
+                cmd = "pdf"
+            elif "drive" in url:
+                cmd = "pdf"
+            elif ytf == "no":
+                cmd = f'yt-dlp -o "{name}.mp4" --no-keep-video --remux-video mp4 "{url}"'
+            else:
+                cmd = f'yt-dlp -f "{ytf}+bestaudio" --hls-prefer-ffmpeg --no-keep-video --remux-video mp4 "{url}" -o "{name}.%(ext)s"'
+                
+
+
+            try:
+                Show = f"**Downloading:-**\n\n**Name :-** `{name}\nQuality - {raw_text2}`\n\n**Url :-** `{url}`\n\n"
+                prog = await m.reply_text(Show)
+                cc = f'**File Name »** {name1} @AS_Multiverse.mp4\n**Batch »** {raw_text0}\n\n**{CR}**'
+                cc1 =f'**File No. »** {str(count).zfill(3)}\n**File Name »** {name1} @AS_Multiverse.pdf\n**Batch »** {raw_text0}\n\n**{CR}**'
+                if cmd == "pdf" or "drive" in url:
+                    try:
+                        ka=await helper.download(url,name)
+                        await prog.delete (True)
+                        time.sleep(1)
+                        # await helper.send_doc(bot,m,cc,ka,cc1,prog,count,name)
+                        reply = await m.reply_text(f"Uploading - `{name}`")
+                        time.sleep(1)
+                        start_time = time.time()
+                        await m.reply_document(ka,caption=cc1)
+                        count+=1
+                        await reply.delete (True)
+                        time.sleep(1)
+                        os.remove(ka)
+                        time.sleep(3)
+                    except FloodWait as e:
+                        await m.reply_text(str(e))
+                        time.sleep(e.x)
+                        continue                    
+                elif cmd == "pdf" or ".pdf" in url:
+                    try:
+                        ka=await helper.aio(url,name)
+                        await prog.delete (True)
+                        time.sleep(1)
+                        reply = await m.reply_text(f"Uploading - ```{name}```")
+                        time.sleep(1)
+                        start_time = time.time()
+                        await m.reply_document(ka, caption=f'**File No. »** {str(count).zfill(3)}\n**File Name »** {name1} @AS_Multiverse.pdf\n**Batch »** {raw_text0}\n\n**{CR}**')
+                        count+=1
+                        # time.sleep(1)
+                        await reply.delete (True)
+                        time.sleep(1)
+                        os.remove(ka)
+                        time.sleep(3)
+                    except FloodWait as e:
+                        await m.reply_text(str(e))
+                        time.sleep(e.x)
+                        continue
+                else:
+                    res_file = await helper.download_video(url,cmd, name)
+                    filename = res_file
+                    await helper.send_vid(bot, m,cc,filename,thumb,name,prog)
+                    count+=1
+                    time.sleep(1)
+
+            except Exception as e:
+                await m.reply_text(f"**downloading failed ❌**\n{str(e)}\n**Name** - {name}\n**Link** - `{url}`")
+                continue
+
+
+    except Exception as e:
+        await m.reply_text(e)
+    await m.reply_text("Done")    
+    
